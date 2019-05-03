05925ad2f667640a481f694dc7f30c87ad1302af
Alex Sahm <sahm.alexander@pwc.com>
2019-05-03 09:31:21 +0200

    add run script

---
+++ run_protoc.sh
@@ -0,0 +1,9 @@
+protoc --proto_path=proto --proto_path=third_party --go_out=plugins=grpc:proto service.proto
+if [ $? -ne 0 ]; then
+    echo "ERROR while creating go stub file!"
+    exit 1
+fi
+
+echo "Go stub file successfully created..."
+ls -rtl proto/service.pb.go
+
