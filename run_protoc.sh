7d48adb1d82f089141b86132503e70f676f7acc7
Alex Sahm <sahm.alexander@pwc.com>
2019-05-03 09:36:47 +0200

    changed location of third party code

--- run_protoc.sh
+++ run_protoc.sh
@@ -1,4 +1,5 @@
-protoc --proto_path=proto --proto_path=third_party --go_out=plugins=grpc:proto service.proto
+protoc --proto_path=proto --proto_path=/usr/local/opt/protoc-3.7.1-osx-x86_64/include --go_out=plugins=grpc:proto service.proto
+
 if [ $? -ne 0 ]; then
     echo "ERROR while creating go stub file!"
     exit 1
