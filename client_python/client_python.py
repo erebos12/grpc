2be62bcd0169102baabb3167fa5f3d0ced2cc9d8
Alex Sahm <sahm.alexander@pwc.com>
2019-05-07 17:11:31 +0200

    renaming and cleanups

--- client_python/client_python.py
+++ client_python/client_python.py
@@ -1,18 +1,15 @@
-import logging
-
 import grpc
 
-import service_pb2_grpc
 import service_pb2
+import service_pb2_grpc
 
 
 def run():
     with grpc.insecure_channel('localhost:4040') as channel:
         stub = service_pb2_grpc.AddServiceStub(channel)
         print("Sending request now...")
-        response = stub.Multiply(service_pb2.Request(a=5, b=4))
+        response = stub.Multiply(service_pb2.Request(a=5, b=25))
         print(f"Response is: {response}")
 
 if __name__ == '__main__':
-    logging.basicConfig()
     run()
