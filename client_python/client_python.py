93161ad14dd4f96d995932c749f0c337c89320b8
Alex Sahm <sahm.alexander@pwc.com>
2019-05-13 09:12:55 +0200

    wrapped a flask app

--- client_python/client_python.py
+++ client_python/client_python.py
@@ -1,15 +1,28 @@
 import grpc
 
+from flask import Flask, jsonify, request
 import service_pb2
 import service_pb2_grpc
+import logging
+
+app = Flask(__name__)
 
 
-def run():
-    with grpc.insecure_channel('localhost:4040') as channel:
-        stub = service_pb2_grpc.AddServiceStub(channel)
-        print("Sending request now...")
-        response = stub.Multiply(service_pb2.Request(a=5, b=25))
-        print(f"Response is: {response}")
+@app.route("/", methods=['GET'])
+def say_hello():
+    return jsonify({"message": "I'm alive!"})
 
-if __name__ == '__main__':
-    run()
+
+@app.route('/multiply/<a>/<b>', methods=['GET'])
+def multiply(a, b):
+    logging.error("python grpc client multiply received {} and {}".format(a, b))
+    try:
+        with grpc.insecure_channel('localhost:4040') as channel:
+            stub = service_pb2_grpc.AddServiceStub(channel)
+            print("Sending request now...")
+            response = stub.Multiply(service_pb2.Request(a=int(a), b=int(b)))
+            print("Response is: {}".format(response.result))
+        return jsonify({"result": response.result})
+    except Exception as e:
+        error = str(e)
+        return jsonify({f"Error": "{}".format(error)})
---
+++ client_python/start_python_grpc_client.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export FLASK_APP=client_python.py
+export FLASK_ENV=development
+python -m flask run --host=0.0.0.0
