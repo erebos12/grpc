3bec3fa4bcaa7f6633bb58a2a54118ff61ee6244
Alex Sahm <sahm.alexander@pwc.com>
2019-05-13 09:49:47 +0200

    changed python client endpoints

--- client_python/client_python.py
+++ client_python/client_python.py
@@ -15,11 +15,10 @@ def say_hello():
     return jsonify({"message": "I'm alive!"})
 
 
-@app.route('/multiply/<a>/<b>', methods=['GET'])
+@app.route('/python/mult/<a>/<b>', methods=['GET'])
 def multiply(a, b):
     app.logger.info(f"python-grpc-client: multiply request received: {a}, {b}")
     try:
-        stub = service_pb2_grpc.AddServiceStub(channel)
         response = stub.Multiply(service_pb2.Request(a=int(a), b=int(b)))
         return jsonify({"result": response.result})
     except Exception as e:
@@ -27,7 +26,7 @@ def multiply(a, b):
         return jsonify({"Error": "{}".format(error)})
 
 
-@app.route('/add/<a>/<b>', methods=['GET'])
+@app.route('/python/add/<a>/<b>', methods=['GET'])
 def add(a, b):
     app.logger.info(f"python-grpc-client: add request received: {a}, {b}")
     try:
