8474efaeb43c1a481b20981fdb37d86e91a9ed83
Alex Sahm <sahm.alexander@pwc.com>
2019-05-13 10:17:53 +0200

    install commands for python grpc

--- client_python/run_protoc_python.sh
+++ client_python/run_protoc_python.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+
+# install grpc: "python -m pip install grpcio"
+# install grpc tools: "python -m pip install grpcio-tools"
+
 python -m grpc_tools.protoc -I../proto --python_out=. --grpc_python_out=. ../proto/service.proto
