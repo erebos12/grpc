fa8cad79be307fb2aedd1ebaab16c1ae2d41ffff
Alex Sahm <sahm.alexander@pwc.com>
2019-05-13 09:26:32 +0200

    changed logging

--- client_python/client_python.py
+++ client_python/client_python.py
@@ -15,14 +15,12 @@ def say_hello():
 
 @app.route('/multiply/<a>/<b>', methods=['GET'])
 def multiply(a, b):
-    logging.error("python grpc client multiply received {} and {}".format(a, b))
+    app.logger.info(f"python-grpc-client: multiply request received: {a}, {b}")
     try:
         with grpc.insecure_channel('localhost:4040') as channel:
             stub = service_pb2_grpc.AddServiceStub(channel)
-            print("Sending request now...")
             response = stub.Multiply(service_pb2.Request(a=int(a), b=int(b)))
-            print("Response is: {}".format(response.result))
         return jsonify({"result": response.result})
     except Exception as e:
         error = str(e)
-        return jsonify({f"Error": "{}".format(error)})
+        return jsonify({"Error": "{}".format(error)})
